d7466c00e3ed1a9f076c7946e90f7c04b541ecd1
t3rickg <efkantunc13@gmail.com>
2024-07-02 22:44:22 +0300

    Update speedtest.py

--- FallenMusic/Modules/speedtest.py
+++ FallenMusic/Modules/speedtest.py
@@ -28,39 +28,39 @@ from pyrogram import filters
 from FallenMusic import SUDOERS, app
 
 
-def testspeed(m):
+def hiztesti(m):
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
-        m = m.edit("**⇆ ʀᴜɴɴɪɴɢ ᴅᴏᴡɴʟᴏᴀᴅ sᴩᴇᴇᴅᴛᴇsᴛ...**")
+        m = m.edit("**⇆ İndirme hız testi yapılıyor...**")
         test.download()
-        m = m.edit("**⇆ ʀᴜɴɴɪɴɢ ᴜᴩʟᴏᴀᴅ sᴩᴇᴇᴅᴛᴇsᴛ...**")
+        m = m.edit("**⇆ Yükleme hız testi yapılıyor...**")
         test.upload()
         test.results.share()
         result = test.results.dict()
-        m = m.edit("**↻ sʜᴀʀɪɴɢ sᴩᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛs...**")
+        m = m.edit("**↻ Hız testi sonuçları paylaşılıyor...**")
     except Exception as e:
         return m.edit(e)
     return result
 
 
 @app.on_message(filters.command(["speedtest", "spt"]) & SUDOERS)
-async def speedtest_function(_, message):
-    m = await message.reply_text("**» ʀᴜɴɴɪɴɢ sᴩᴇᴇᴅᴛᴇsᴛ...**")
+async def hiztesti_fonksiyonu(_, message):
+    m = await message.reply_text("**» Hız testi yapılıyor...**")
     loop = asyncio.get_event_loop()
-    result = await loop.run_in_executor(None, testspeed, m)
-    output = f"""✯ **sᴩᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛs** ✯
+    result = await loop.run_in_executor(None, hiztesti, m)
+    output = f"""✯ **Hız Testi Sonuçları** ✯
     
-<u>**❥͜͡ᴄʟɪᴇɴᴛ :**</u>
-**» __ɪsᴩ :__** {result['client']['isp']}
-**» __ᴄᴏᴜɴᴛʀʏ :__** {result['client']['country']}
+<u>**❥͜͡Müşteri :**</u>
+**» __İSS :__** {result['client']['isp']}
+**» __Ülke :__** {result['client']['country']}
   
-<u>**❥͜͡sᴇʀᴠᴇʀ :**</u>
-**» __ɴᴀᴍᴇ :__** {result['server']['name']}
-**» __ᴄᴏᴜɴᴛʀʏ :__** {result['server']['country']}, {result['server']['cc']}
-**» __sᴩᴏɴsᴏʀ :__** {result['server']['sponsor']}
-**» __ʟᴀᴛᴇɴᴄʏ :__** {result['server']['latency']}  
-**» __ᴩɪɴɢ :__** {result['ping']}"""
+<u>**❥͜͡Sunucu :**</u>
+**» __İsim :__** {result['server']['name']}
+**» __Ülke :__** {result['server']['country']}, {result['server']['cc']}
+**» __Sunucu :__** {result['server']['sponsor']}
+**» __Gecikme :__** {result['server']['latency']}  
+**» __Ping :__** {result['ping']}"""
     msg = await app.send_photo(
         chat_id=message.chat.id, photo=result["share"], caption=output
     )
