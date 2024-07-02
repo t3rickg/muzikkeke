5a7ac03a3d4c2a053a9470927fb57437b2eac68e
t3rickg <efkantunc13@gmail.com>
2024-07-02 22:12:25 +0300

    Update play.py

--- FallenMusic/Modules/play.py
+++ FallenMusic/Modules/play.py
@@ -60,7 +60,7 @@ from FallenMusic.Helpers.thumbnails import gen_qthumb, gen_thumb
 
 
 @app.on_message(
-    filters.command(["play", "vplay", "p"])
+    filters.command(["oynat", "vplay", "p"])
     & filters.group
     & ~filters.forwarded
     & ~filters.via_bot
