5b4e503706095f2382720a86abacddea05b579a4
t3rickg <efkantunc13@gmail.com>
2024-08-08 14:35:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://c.tenor.com/_yFLs1OWgBAAAAAC/tenor.gif")
-START_IMG = getenv("START_IMG", "https://c.tenor.com/_yFLs1OWgBAAAAAC/tenor.gif")
+PING_IMG = getenv("PING_IMG", "https://i.hizliresim.com/ebszw3y.jpg")
+START_IMG = getenv("START_IMG", "https://i.hizliresim.com/ebszw3y.jpg")
 
 SESSION = getenv("SESSION", None)
 
@@ -24,4 +24,4 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/tekornetwork")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7490336041").split()))
 
 
-FAILED = "https://i.hizliresim.com/p9rb864.png"
+FAILED = "https://i.hizliresim.com/ebszw3y.jpg"
