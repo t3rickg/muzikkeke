f354cba69ebec050f9cd051b2b12a932efc61a5c
t3rickg <efkantunc13@gmail.com>
2024-08-08 16:19:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = '22659994'
-API_HASH = '2c89964a0088a7a39ec819c60ae67de7'
-BOT_TOKEN = '5909970619:AAGbiFzsH_Owy350MUmHhdPDQsPddEr-0Xw'
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
 
+BOT_TOKEN = getenv("BOT_TOKEN", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = getenv("6563936773")
+OWNER_ID = int(getenv("OWNER_ID"))
 
 PING_IMG = getenv("PING_IMG", "https://i.hizliresim.com/ebszw3y.jpg")
 START_IMG = getenv("START_IMG", "https://i.hizliresim.com/ebszw3y.jpg")
